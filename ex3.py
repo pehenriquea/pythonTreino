d0c8022b09113adab1105267a48b447900784799
Pedro Santos <99020483+pehenriquea@users.noreply.github.com>
2022-11-19 11:50:49 -0300

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,12 @@
+#Faça um Programa que peça as 4 notas
+#bimestrais e mostre a média.
+
+print("Digite uma nota: ")
+nota1 = float(input())
+print("Digite uma nota: ")
+nota2 = float(input())
+print("Digite uma nota: ")
+nota3 = float(input())
+print("Digite uma nota: ")
+nota4 = float(input())
+print("A média é igual a "+ str((nota1+nota2+nota3+nota4)/4))

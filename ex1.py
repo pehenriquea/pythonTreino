1b7f96181259130df43d6ddef4b48585ca9fa479
Pedro Santos <99020483+pehenriquea@users.noreply.github.com>
2022-11-19 11:45:40 -0300

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,6 @@
+#Faça um Programa que peça um número e
+#então mostre a mensagem O número informado foi [número].
+
+print("Digite um numero: ")
+numero = input()
+print("O número informado foi "+ numero)
